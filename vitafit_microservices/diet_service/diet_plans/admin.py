5467a9f190529ae329196d2d931143e086fb29e8
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2026-03-28 12:40:48 +0530

    completed diet-service

--- vitafit_microservices/diet_service/diet_plans/admin.py
+++ vitafit_microservices/diet_service/diet_plans/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import DietPlan
 
-# Register your models here.
+@admin.register(DietPlan)
+class DietPlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'meal_type', 'calories', 'protein', 'carbs', 'fats')
+    list_filter = ('meal_type',)
+    search_fields = ('name', 'description')
